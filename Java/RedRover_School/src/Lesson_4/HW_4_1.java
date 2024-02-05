dd415fdbb522e1bafa00685806e8f1dd4136df77
Aleksei Nazarov <naz.arrow.lex@gmail.com>
2024-02-05 21:42:14 +0300

    added HW 4_1 - 4_3

---
+++ Java/RedRover_School/src/Lesson_4/HW_4_1.java
@@ -0,0 +1,11 @@
+package Lesson_4;
+
+//Необходимо вывести числа от 0 до 15.
+
+public class HW_4_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 16; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Java/RedRover_School/src/Lesson_4/HW_4_2.java
@@ -0,0 +1,12 @@
+package Lesson_4;
+
+//Необходимо вывести все положительные степени числа 5 которые меньше 10000, вывести результат возведения в степень.
+
+public class HW_4_2 {
+
+    public static void main(String[] args) {
+        for (int i = 5; i < 10000; i = i * 5) {
+            System.out.println(i);
+        }
+    }
+}
