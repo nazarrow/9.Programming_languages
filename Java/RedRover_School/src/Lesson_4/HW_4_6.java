70b89535032d021d4b88d6e30370fe653316fc13
Aleksei Nazarov <naz.arrow.lex@gmail.com>
2024-02-06 20:31:08 +0300

    added HW 4_4 - 4_6

---
+++ Java/RedRover_School/src/Lesson_4/HW_4_6.java
@@ -0,0 +1,18 @@
+package Lesson_4;
+
+/*Дан массив:
+int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+необходимо увеличить все значения массива на 15.*/
+
+import java.util.Arrays;
+
+public class HW_4_6 {
+
+    public static void main(String[] args) {
+        int[] array = {9, 2, 6, 4, 5, 12, 7, 8, 6};
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] + 15;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
